c62808a8a21ef66037add19147601b0166a58a51
calepayson <calepayson@mac.com>
2023-12-24 13:02:26 -0800

    Calculates the nth Fibonacci number

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,34 @@ fn main() {
     let n: u32 = n
         .trim()
         .parse()
-        .expect("Please input a number");
+        .expect("Please input a positive number");
 
-    println!("n * 2 = {}", n * 2);
+    let x = calculate_nth_fibonacci(n);
+
+    println!("x = {x}");
+}
+
+fn calculate_nth_fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        0
+    } else if n == 1 || n == 2 {
+        1
+    } else {
+        let mut current = 1;
+        let mut previous = 1;
+
+        let mut count = 0;
+        loop {
+            if count == (n - 2){
+                break
+            } else {
+                let memory = current;
+                current = current + previous;
+                previous = memory;
+
+                count += 1;
+            }
+        }
+        current
+    }
 }
